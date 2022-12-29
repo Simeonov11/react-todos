df9d424722503aec76d4893809e8d58af482fca9
Simo <simeonov.pa@gmail.com>
2022-12-29 17:47:55 +0000

    Event props

--- todos-app/src/components/Todo.js
+++ todos-app/src/components/Todo.js
@@ -10,6 +10,9 @@ function Todo(props) {
         setModalIsOpen(true);
     }
 
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
 
     return (
         <div className="card">
@@ -17,8 +20,8 @@ function Todo(props) {
         <div className="actions">
           <button className="btn" onClick={deleteHandler}>Delete</button>
         </div>
-        { modalIsOpen && <Modal /> }  {/*if modalIsOpen AND Modal are True will return the second component and render it*/}
-        { modalIsOpen && <Backdrop /> }
+        { modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} /> }  {/*if modalIsOpen AND Modal are True will return the second component and render it*/}
+        { modalIsOpen && <Backdrop onCancel={closeModalHandler} /> }
         
       </div>
     )
